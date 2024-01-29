c15492bb1d3d454622b912bbb7bb527fd6daab93
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-29 15:35:20 +0800

    create recipe schema

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+mongoose.set("strictQuery", false);
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: "Your New Recipe Title",
+      level: "Easy Peasy",
+      ingredients: ["Ingredient 1", "Ingredient 2"],
+      cuisine: "Your Cuisine",
+      dishType: "main_course",
+      image:
+        "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      duration: 30,
+      creator: "Your Name",
+    };
+
+    return Recipe.create(newRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((createdRecipe) => {
+    console.log(`Recipe created with title: ${createdRecipe.title}`);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
